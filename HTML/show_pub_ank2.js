2e3326f3058bb764fcb78ae4ab41a5d29b989af2
kirillvladykin <kirillmybox@rambler.ru>
2017-12-11 22:02:36 +0300

    - init

---
+++ HTML/show_pub_ank2.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+alert("READY")
+  if ($("#mode_show").val()==0) {
+//PUB - режим просмотра
+    $("#ank_sum_balls").html("");
+    $("#contact_owner").html("");
+    $(".inf_butt").html("");
+    $(".num_balls").parent().html(""); 
+    $(".ball_after_check").html("");
+    $(".ball_first").remove();
+    $(".ball_last").remove();
+    $(".str_act").remove();
+  }
+ return;
+});
